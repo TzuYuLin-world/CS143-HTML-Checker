626f11e6712dffe841caee9e1136c6105c145007
TzuYuLin-world <angelinalin0802@gmail.com>
2026-05-20 12:08:17 -0700

    Update HTMLManager.java

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,7 +3,12 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
 
-  public HTMLManager(Queue<HTMLTag> html) {}
+  public HTMLManager(Queue<HTMLTag> html) {
+    if(html = null) {
+      throw new IllegalArgumentException();
+    }
+    
+  }
 
   public Queue<HTMLTag> getTags() {}
 
