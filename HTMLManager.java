e6bb36dcbe85421df4e33d638a97ffa3cba2fbf3
TzuYuLin-world <angelinalin0802@gmail.com>
2026-05-23 16:54:07 -0700

    Update HTMLManager.java

--- HTMLManager.java
+++ HTMLManager.java
@@ -7,7 +7,9 @@ public class HTMLManager {
     if(html == null) {
       throw new IllegalArgumentException();
     }
+    
     tags = new LinkedList<HTMLTag>();
+    
     for(HTMLTag tag : html) {
       tags.add(tag);
     }
@@ -21,14 +23,52 @@ public class HTMLManager {
   public String toString() {
     String result = "";
     int size = tags.size();
+    
     for(int i = 0; i < size; i++) {
       HTMLTag tag = tags.remove();
       return += tag.toString.trim();
+      tags.add();
     }
+
+    return result;
     
   }
 
-  public void fixHTML() {}
+  public void fixHTML() {
+    Stack<HTMLTag> stack = new Stack<HTMLTag>();
+    Queue<HTMLTag> fixed = new LinkedList<HTMLTag>();
+
+    while(!tags.isEmpty()) {
+      HTMLTag tag = tags.remove();
+      
+      if(tag.isSelfClosing()) {
+        fixed.add(tag);
+      } else if(tag.isOpening()) {
+        fixed.add();
+        stack.push();
+      } else if(tag.isClosing()) {
+        if(!stack.isEmpty()) {
+          HTMLTag top = stack.peek();
+
+          if(top.matches()) {
+            fixed.add(tag);
+            stack.pop();
+          } else {
+            fixed.add(top.getMatching());
+            stack.pop();
+            tags.add(tag);
+          }
+        }
+      }
+        
+    }
+    while(!stack.isEmpty()) {
+      fixed.add(stack.pop().getMatching());
+    }
+
+    tags = fixed;
+
+  }
 
   
 
